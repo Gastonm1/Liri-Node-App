b7c8938fb70b3e066f95bac0a8e5e6d5b1f87a8a
Gastonm1 <gaston.marques@gmail.com>
2019-02-10 19:49:23 -0600

    songs, movies

--- liri.js
+++ liri.js
@@ -19,7 +19,10 @@ switch (command) {
         return console.log("Error occurred: " + err);
       }
 
-      console.log(data.tracks);
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song: " + data.tracks.items[0].name);
+      console.log("Check out the preview: " + data.tracks.items[0].preview_url);
+      console.log("Album Name: " + data.tracks.items[0].album.name);
     });
     break;
 
@@ -30,16 +33,53 @@ switch (command) {
       "https://rest.bandsintown.com/artists/" +
       artist +
       "/events?app_id=codingbootcamp";
+      
 
     axios.get(url).then(function(response) {
-      console.log(response.data);
+      console.log("Venue Name: " + response.data[0].venue.name);
+      console.log("Country: " + response.data[0].venue.country);
+      console.log("City: " + response.data[0].venue.city);
+      console.log("Time: " + response.data[0].datetime); // < --- NEED TO USE MOMENT!!!!!!
     });
     break;
 
   case "movie-this":
+    var movieName = process.argv.slice(3).join(" ");
+
+    var url = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    
+    axios.get(url).then(
+      function(response) {
+        //console.log(response)
+        console.log("Title: " + response.data.Title);
+        console.log("Release Year: " + response.data.Year);
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("Country Produced: " + response.data.Country);
+        console.log("Language of the Movie: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Country Produced: " + response.data.Country);
+        console.log("A few of the Actors : " + response.data.Actors);
+      });
+
+    //   if( process.argv.slice(3).join(" ") === " "){    // <-- Mr.Nobody
+    //     =
+    //   }
+    // );
+
     break;
 
   case "do-what-it-says":
+      var fs = require('fs');
+
+      fs.readFile('random.txt', 'utf8', function(err, data){
+        if (err) {
+          return console.log(err);
+        }
+        var output = data;
+        console.log(output)
+      })
+
     break;
 
   default:
